f88a32a60bb9caffaa879458cfd9b60bd29caa4e
Ceyda Ünal <ceydun06@gmail.com>
2026-03-29 22:39:59 +0300

    service testi

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.entity.Calisan;
 import com.example.demo.repository.CalisanRepository;
+import com.example.demo.service.CalisanService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,17 +15,44 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+//    @Bean
+//    public CommandLineRunner ilkCalisanlariEkle(CalisanRepository repository) {
+//        return args -> {
+//            Calisan calisan1 = new Calisan("Ahmet", "Yilmaz",45000.0);
+//            repository.save(calisan1);
+//
+//            Calisan calisan2 = new Calisan("Asyse", " Demir", 52.500);
+//            repository.save(calisan2);
+//
+//            System.out.println("--- ÇALIŞANLAR VERİTABANINA BAŞARIYLA EKLENDİ! ---");
+//
+//        };
+//    }
+
+
     @Bean
-    public CommandLineRunner ilkCalisanlariEkle(CalisanRepository repository) {
+    public CommandLineRunner servisTesti(CalisanService calisanService) {
         return args -> {
-            Calisan calisan1 = new Calisan("Ahmet", "Yilmaz",45000.0);
-            repository.save(calisan1);
+            System.out.println("--- SERVİS KATMANI TESTİ BAŞLIYOR ---");
 
-            Calisan calisan2 = new Calisan("Asyse", " Demir", 52.500);
-            repository.save(calisan2);
+            try {
+                // TEST 1: Geçerli bir çalışan ekle
+                Calisan c1 = new Calisan("Mehmet", "Öz", 25000.0);
+                calisanService.calisanKaydet(c1);
+                System.out.println("Başarılı: Mehmet eklendi.");
 
-            System.out.println("--- ÇALIŞANLAR VERİTABANINA BAŞARIYLA EKLENDİ! ---");
+                // TEST 2: Hatalı (Düşük Maaşlı) bir çalışan ekle
+                Calisan c2 = new Calisan("Hatalı", "Veri", 5000.0);
+                calisanService.calisanKaydet(c2); // Burada program durup hataya düşecek
 
+            } catch (Exception e) {
+                System.out.println("Yakaladığımız Hata: " + e.getMessage());
+            }
+
+            // TEST 3: Listeleme
+            System.out.println("\nGüncel Çalışan Listesi:");
+            calisanService.tumCalisanlariGetir().forEach(c ->
+                    System.out.println(c.getAd() + " - Maaş: " + c.getMaas()));
         };
     }
 }
